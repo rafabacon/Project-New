2cfeb96fb558ddf47924ee5bd4b8d583434a42dc
Rafael Baccon <rafabacon@yahoo.com.br>
2017-10-12 16:50:20 -0300

    Add files via upload

---
+++ Atividade rafael.R
@@ -0,0 +1,35 @@
+﻿## Aluno: Rafael Baccon - 71624368
+## Faculdade: Universidade Prebisteriana Mackenzie
+## Curso: Big Data
+## Matéria: Mineração de Dados
+## Professores: Diego Nogare e Rogerio de Oliveira
+
+
+## Utlizei o caret por já possuir varias opções na biblioteca 
+
+url<-"https://archive.ics.uci.edu/ml/machine-learning-databases/voting-records/house-votes-84.data"
+
+df<-read.table(url, fileEncoding="UTF-8", sep=",")
+
+library(caret)
+
+set.seed(1984)
+
+## Divide as bases
+btrain <- createDataPartition(y = df$V1, p= 0.7, list = FALSE)
+
+training <- df[btrain,]
+testing <- df[-btrain,]
+
+
+## Roda o modelo
+svm<-train(V1~.,
+              data=training,
+              method="svmLinear")
+
+
+## cria a matriz de confusão
+
+p_svm<-predict(svm,testing)
+pp_svm<-confusionMatrix(p_svm,testing$V1)
+pp_svm
